e861b9195fbbe934ab7683e6e716e3d93e5a3e17
JayDub21 <justin.wofford.21@gmail.com>
2020-01-07 10:26:09 -0600

    added forecast into console

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,28 +1,56 @@
+var weatherAPI = '46787f6b49aba46062b95df0b46828d9';
+
 $(document).ready(function () {
 
+    var austinEl = $("#austin");
+    var chicagoEl = $("#chicago");
+    var newYorkEl = $("#newYork");
+    var orlandoEl = $("#orlando");
+    var sanFranciscoEl = $("#sanFrancisco");
+    var seattleEl = $("#seattle");
+    var denverEl = $("#denver");
+    var atlantaEl = $("#atlanta");
 
+    var citySearchBar = $("#citySearch");
+    var searchBtn = $("#searchBtn");
     var curWeather = $('#curWeather');
-    var cityInput = document.getElementById("cityInput").value;
 
 
-    console.log(cityInput);
 
     // Creating an AJAX call for the api link above.
-    $('#cityInput').on('click', function () {
+    $('#searchBtn').on('click', function () {
+        console.log(citySearchBar.val());
 
         $.ajax({
-
-            url: "http://api.openweathermap.org/data/2.5/weather?id=524901&q=" + cityInput + "&units=imperial" + "&APPID=46787f6b49aba46062b95df0b46828d9",
-
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + citySearchBar.val() + "&units=imperial&appid=" + weatherAPI,
             method: "GET"
         }).then(function (response) {
-
             console.log(response);
 
-            $('#searchBtn').on('click', function (cityInput) {
-                curWeather.html(response.main.temp);
-            });
+            // Display City Name
+            var cityName = response.name;
+            $("#cityName").html(cityName);
+
+            // Display City Temp
+            var cityTemp = response.main.temp;
+            $("#curTemp").html("Temperature: " + cityTemp + "&deg;F");
+
+            // Display City Humidity
+            var cityHumidity = response.main.humidity;
+            $("#humidityIn").html("Humidity: " + cityHumidity + "%");
+
+            // Display City Wind
+            var cityWind = response.wind.speed;
+            $("#windIn").html("Wind: " + cityWind + " m.p.h.");
         })
+
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearchBar.val() + "&units=imperial&appid=" + weatherAPI,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        })
+
     });
 
 });
@@ -30,3 +58,6 @@ $(document).ready(function () {
 
 
 
+
+
+// citySearchBar.val() + 
